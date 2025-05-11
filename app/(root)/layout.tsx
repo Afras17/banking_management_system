f5c067fba0adcd846e424ba5666a9cb4f2ffc797
Afras Rahim <afrasrahim11@gamil.com>
2025-05-12 02:16:20 +0530

    file and folder structure initialized

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main className="flex h-screen w-full font-inter">
+           SIDEBAR
+           {children} 
+        </main>
+    );
+}
